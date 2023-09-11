c43491fff6c27c611ff3f60ac2e4b661c7dced76
gajaharan <gajaharan@gmail.com>
2023-09-11 21:42:10 +0100

    [GAJ-01] init commit

---
+++ src/main/java/com/phorest/PhorestTechtestGajaharanSatkunanandanApplication.java
@@ -0,0 +1,13 @@
+package com.phorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhorestTechtestGajaharanSatkunanandanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhorestTechtestGajaharanSatkunanandanApplication.class, args);
+	}
+
+}
